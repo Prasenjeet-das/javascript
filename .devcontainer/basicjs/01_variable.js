7c6b24d19c9fb790bbb1b83b5a023e1b7f5925eb
Prasanjeet Das <165247108+Prasenjeet-das@users.noreply.github.com>
2025-02-11 16:38:27 +0000

    configration

---
+++ .devcontainer/basicjs/01_variable.js
@@ -0,0 +1,11 @@
+const accountId =1234
+let accountEmail = "das@gamil.com"
+var accountPassword = "112222"
+accountCity = "Patna"
+
+//accountId = 2 //it is no change
+
+accountEmail = "abc@gmail.com"
+accountId = "33344334"
+accountCity = " punjab"
+console.table([ccountId, accountEmail, accountPassword, accountCity])
