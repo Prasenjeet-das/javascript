ff5dcbc21468110515c1565d1fbb502733d6e2c1
Prasanjeet Das <165247108+Prasenjeet-das@users.noreply.github.com>
2025-02-13 17:38:54 +0000

    configration

--- .devcontainer/basicjs/02_datatype.js
+++ .devcontainer/basicjs/02_datatype.js
@@ -1,4 +1,21 @@
 "use strict "; // treat all js code newer version
 // alert( 3 + 3) // we are using nodejs not brower
-console.log(3 + 3);
-console.log("Prasenjeet");
+console.log(3 + 3)
+console.log("Prasenjeet")
+let name = "prasenjeet"
+let age = 24
+let isLoggedId = false
+let state;
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standa lone value
+// undefrined =>
+// symbol => unique
+
+// object
+console.log(typeof Number);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
